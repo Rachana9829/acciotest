cb45d93b1a6280d8300e802ec0e7dea8057233b0
rachana9829 <rachana.acharya@niveussolutions.com>
2022-06-06 23:56:24 +0530

    scripts (deleted)

--- scripts/wait-for-it.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1edd2abf0891f7add0e283cabf4723b4f4debcc65ffab4fea8e0203d9d4d0206
-size 4050
--- scripts/waitforit.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:96a572e3afaf524e763bf6aa896dbf8f52b0b1d4e08faab9c53826966954eb1a
-size 4051
