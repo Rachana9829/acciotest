973a9901f56c405ec83a55bc72868e073bf513d3
rachana9829 <rachana.acharya@niveussolutions.com>
2022-06-06 23:52:45 +0530

    tests

---
+++ tests/config/globalSetup.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:85916f304b5e5532ff1998f7c1a11186d3f4f6e8fde9dbe0f0661ed2bc765846
+size 1658
---
+++ tests/config/playwright.config.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f71861019685c20091a5fa0cd2626e6ad7f5c032c50a567ed86bfd818c476356
+size 2246
